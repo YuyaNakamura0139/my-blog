5524228fccabdd452f50f42dd740fcfcc2689f3f
YuyaNakamura0139 <mtk371@icloud.com>
2022-02-26 18:43:07 +0900

    #1 settings.pyの修正

--- myblog/settings.py
+++ myblog/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = "myblog.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "tech_blog",
+        "NAME": "my_blog",
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
         "HOST": "",
@@ -123,8 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
